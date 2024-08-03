087353f96264b4361387cf7875aebb91fb68fe11
Nasri Rusni <nasrirusni1@gmail.com>
2024-08-03 15:14:49 +0800

    revoked key

--- lib/controller/model_controller.dart
+++ lib/controller/model_controller.dart
@@ -3,6 +3,6 @@ import 'package:google_generative_ai/google_generative_ai.dart';
 class ModelController {
   final model = GenerativeModel(
       model: 'gemini-1.5-flash-latest',
-      apiKey: 'AIzaSyAENAF3xHDy5dljlDu8VZuTo8He61ixU6k',
+      apiKey: '',
       generationConfig: GenerationConfig(responseMimeType: "application/json"));
 }
